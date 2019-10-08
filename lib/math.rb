fe2bd3bb51415b5998140aab141a248c531ee36f
Romario Gustave <romagustave10@gmail.com>
2019-10-08 06:24:09 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,29 +1,31 @@
 def addition(num1, num2)
-  addition = num1 + num2
+  addition == num1 + num2
   puts addition
 end
 
-def subtraction(num1=5, num2=4)
-  subtraction= num2 - num1
+def subtraction(num1, num2)
+  num1= 5
+  num2= 4
+  subtraction == num2 - num1
   puts subtraction
 end
 
 def division(num1, num2)
-  division = num2/num1
+  division == num2/num1
   puts division
 end
 
 def multiplication(num1, num2)
-  multiplication = num1*num2
+  multiplication == num1*num2
   puts multiplication
 end
 
 def modulo(num2, num1)
-  modulo= num1 % num2
+  modulo == num1 % num2
   puts modulo
 end
 
 def square_root(num)
-  square_root = Math.sqrt num
+  square_root == Math.sqrt num
   puts square_root
 end
